f011f810c3ef3cf223f6b04a4821c62a9fb40b19
Julia <berrylily.0709@gmail.com>
2017-05-26 16:51:26 -0700

    Implement true/false button and toast

--- app/src/main/java/com/test/truefalsequizapp/MainActivity.java
+++ app/src/main/java/com/test/truefalsequizapp/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     private Button mHintBtn;
 
@@ -14,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // trueBtn
+        mTrueButton = (Button) findViewById(R.id.trueBtn);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        // falseBtn
+        mFalseButton = (Button) findViewById(R.id.falseBtn);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
 
         // hintBtn: go to HintActivity
         mHintBtn = (Button) findViewById(R.id.hintBtn);
@@ -24,6 +46,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(goToHintActivity);
             }
         });
-
     }
+
+    // toast: correct or incorrect
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = true;
+        int messageResId = 0;
+        if (answerIsTrue == userPressedTrue) {
+            messageResId = R.string.correctToast;
+        } else {
+            messageResId = R.string.incorrectToast;
+        }
+
+        Toast.makeText(MainActivity.this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
 }
