e10578ba9c1c6d7633c9796e4e98c2d678905eeb
Julia <berrylily.0709@gmail.com>
2017-05-26 15:12:38 -0700

    Implement Activity transitions between Main and Hint

--- app/src/main/java/com/test/truefalsequizapp/HintActivity.java
+++ app/src/main/java/com/test/truefalsequizapp/HintActivity.java
@@ -1,4 +1,25 @@
 package com.test.truefalsequizapp;
 
-public class HintActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HintActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hint);
+
+        // backBtn: go back to MainActivity
+        Button returnButton = (Button) findViewById(R.id.backBtn);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
 }
