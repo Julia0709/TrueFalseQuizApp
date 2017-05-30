3691704c455960b4f8671b2d0cbbf8ac8e8906e3
Julia <berrylily.0709@gmail.com>
2017-05-29 22:13:43 -0700

    Implement showAnswerBtn

--- app/src/main/java/com/test/truefalsequizapp/HintActivity.java
+++ app/src/main/java/com/test/truefalsequizapp/HintActivity.java
@@ -1,25 +1,60 @@
 package com.test.truefalsequizapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HintActivity extends Activity {
+
+    private TextView hShowAnswerText;
+    private TextView hAnswerText;
+    private boolean hAnswerTrue = true; // need to implement
+
+    // buttons
+    private Button hGoBackBtn;
+    private Button hShowAnswerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hint);
 
-        // backBtn: go back to MainActivity
-        Button returnButton = (Button) findViewById(R.id.backBtn);
-        returnButton.setOnClickListener(new View.OnClickListener() {
+        // showAnswerText: set text color
+        hShowAnswerText = (TextView) findViewById(R.id.showAnswerText);
+        hShowAnswerText.setTextColor(Color.DKGRAY);
+
+        // answerText: display answer
+        hAnswerText = (TextView) findViewById(R.id.answerText);
+
+        // showAnswerBtn
+        hShowAnswerBtn = (Button) findViewById(R.id.showAnswerBtn);
+        hShowAnswerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateAnswer();
+            }
+        });
+
+        // goBackBtn: go back to MainActivity
+        hGoBackBtn = (Button) findViewById(R.id.goBackBtn);
+        hGoBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-
     }
+
+    // update answer
+    private void updateAnswer() {
+        if (hAnswerTrue) {
+            hAnswerText.setText(R.string.trueBtn);
+        } else {
+            hAnswerText.setText(R.string.falseBtn);
+        }
+    }
+
 }
